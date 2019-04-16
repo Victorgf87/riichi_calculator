d5ce06984a3512d680a20ed19dda9d039eba8ebf
Víctor M. Gonzalez Fuente <victor.gf87@gmail.com>
2019-04-16 02:38:04 +0200

    Okey

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Picker, Switch} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {Platform, StyleSheet, Text, View, TextInput, Picker, Switch, Button, Alert} from 'react-native';
 
 
 export default class App extends Component{
@@ -17,6 +10,8 @@ export default class App extends Component{
     super(props);
     this.state = {fu: 20, han: 1, handValue: 0, oyaValue: 0, koValue: 0, oya: false}
     this.calculate = this.calculate.bind(this);
+    this.name = this.name.bind(this);
+    this.showFuHelp = this.showFuHelp.bind(this);
   }
 
   calculate(fu, han, oya)
@@ -38,10 +33,6 @@ export default class App extends Component{
       else if(han <=12)
       {
         value = 24000
-      }
-      else if(han <=13)
-      {
-        value = 32000;
       }
     }
     else
@@ -72,52 +63,91 @@ export default class App extends Component{
   {
     this.calculate(this.state.fu, this.state.han);
   }
+  name()
+  {
+    if(this.state.han <5)
+    {
+      return "";
+    }
+    else if(this.state.han == 5)
+    {
+      return "満貫 Mangan"
+    }
+    else if(this.state.han <=7)
+    {
+      return "跳満 Haneman"
+    }
+    else if (this.state.han <=10)
+    {
+      return "倍満 Baiman";
+    }
+    else if(this.state.han <=12)
+    {
+      return "三倍満 Sanbaiman"
+    }
+  }
+
+  showFuHelp()
+  {
+    Alert.alert("符 Fu Helper:", "Titulo")
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <View >
-          <Switch
-          value = {this.state.oya}
-          onValueChange={(value)=>{
-            this.setState({oya: !this.state.oya});
-            this.calculate(this.state.fu, this.state.han, value);
-          }}
-          />
-        <Text>Fu</Text>
-          <Picker
-            selectedValue={this.state.fu}
-            style={{ height: 50, width: 100 }}
-            onValueChange={(itemValue, itemIndex) =>{
-              this.setState({ fu: itemValue });
-              this.calculate(itemValue, this.state.han, this.state.oya);
-            }
-
-            }>
-            {[20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110].map(value =>{return <Picker.Item key={`fu_${value}`}
-              label={`${value}`} value={value} />})}
-          </Picker>
+        <View style={styles.row}>
+          <Button
+            title='Fus'
+            onPress={this.showFuHelp} />
         </View>
-        <View >
-        <Text>Han</Text>
-          <Picker
-            selectedValue={this.state.han}
-            style={{ height: 50, width: 100 }}
-            onValueChange={(itemValue, itemIndex) =>{
-              this.setState({ han: itemValue });
-              this.calculate(this.state.fu, itemValue, this.state.oya);
-            }
-            }>
-            {([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]).map(value => {
-              return <Picker.Item key={`han_${value}`}
-                label={`${value}`} value={value} />
-            })}
+        <View style={styles.row}>
+          <Text>Dealer?</Text>
+          <Switch
+            value={this.state.oya}
+            onValueChange={(value) => {
+              this.setState({ oya: !this.state.oya });
+              this.calculate(this.state.fu, this.state.han, value);
+            }}
+          />
 
-          </Picker>
+        </View>
+        <View style={styles.row}>
+            <View style={styles.container}>
+              <Text>Fu</Text>
+              <Picker
+                selectedValue={this.state.fu}
+                style={{ height: 50, width: 100 }}
+                onValueChange={(itemValue, itemIndex) =>{
+                  this.setState({ fu: itemValue });
+                  this.calculate(itemValue, this.state.han, this.state.oya);
+                }
+
+                }>
+                {[20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110].map(value =>{return <Picker.Item key={`fu_${value}`}
+                  label={`${value}`} value={value} />})}
+              </Picker>
+            </View>
+            <View style={styles.container}>
+              <Text>Han</Text>
+              <Picker
+                selectedValue={this.state.han}
+                style={{ height: 50, width: 100 }}
+                onValueChange={(itemValue, itemIndex) =>{
+                  this.setState({ han: itemValue });
+                  this.calculate(this.state.fu, itemValue, this.state.oya);
+                }
+                }>
+                {([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]).map(value => {
+                  return <Picker.Item key={`han_${value}`}
+                    label={`${value}`} value={value} />
+                })}
+
+              </Picker>
+            </View>
         </View>
         <View>
-
-          <Text>
+          <Text style={styles.name}>{this.name()}</Text>
+          <Text style={styles.score}>
             {this.state.handValue}{(this.state.oya) ? `(${this.state.koValue})` : `(${this.state.oyaValue} / ${this.state.koValue})`}
           </Text>
         </View>
@@ -129,19 +159,20 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    padding: 20,
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  row: {
+    margin: 20,
+    flexDirection: 'row',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  name: {
+    fontSize: 35
   },
+  score: {
+    fontSize: 35
+  }
 });
